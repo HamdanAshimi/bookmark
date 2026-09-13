8dd893b6a23ab1a33ae5530a2ae2012614f73d7f
HamdanAshimi <ashimiademola4@gmail.com>
2026-09-14 00:52:36 +0100

    Add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import HeroSection from "./components/HeroSection.jsx";
 import Nav from "./components/Nav.jsx";
 
 function App() {
   return (
     <>
       <Nav />
+      <HeroSection />
     </>
   );
 }
