cd31501ad803d69bed4da76a24453e6c7d862ddb
HamdanAshimi <ashimiademola4@gmail.com>
2026-09-14 00:57:36 +0100

    Add features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "./components/HeroSection.jsx";
 import Nav from "./components/Nav.jsx";
+import Features from "./components/Features.jsx";
 
 function App() {
   return (
     <>
       <Nav />
       <HeroSection />
+      <Features />
     </>
   );
 }
